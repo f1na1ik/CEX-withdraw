48690ee3e1d6ff7abbbe6540b8e425ce8e1266c4
f1na1ik <maxsim119@yandex.ru>
2023-07-22 20:11:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+class WithdrawApp:
+    def __init__(self):
+
+
+
+
+if __name__ == '__main__':
+    root.mainloop()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
